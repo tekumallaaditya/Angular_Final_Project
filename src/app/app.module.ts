7eae04d32e28ed7483e9a824cfd6b46cc28d7c46
Aditya Tekumalla <tekumallaaditya@gmail.com>
2018-06-23 23:13:03 +0200

    added Admin and User components. also added Navbar along with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './users/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'admin', component:AdminComponent},
+      {path:'user', component:UserComponent}
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/users/user.component.ts
@@ -0,0 +1,10 @@
+import{Component} from '@angular/core';
+
+@Component({
+    selector:'user-app',
+    templateUrl:'./user.component.html'
+})
+
+export class UserComponent{
+    
+}
